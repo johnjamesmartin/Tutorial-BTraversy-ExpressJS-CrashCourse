28da6c97c77d402db97d5b63b7008247adb81622
“John <johnjamesmartin@live.com>
2016-10-21 03:12:34 +0100

    Template engine - ejs

--- customer-app/app.js
+++ customer-app/app.js
@@ -7,11 +7,61 @@ var path = require('path');
 
 var app = express();
 
+/* 
+var logger = function(req, res, next) {
+    console.log('Logging...');
+    next();
+}
+
+app.use(logger);
+*/
+
+
+/* View engine: */
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+/* Body parser middleware: */
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+/* Set static path: */
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@gmail.com'
+    },
+    {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bobsmith@gmail.com'
+    },
+    {
+        id: 3,
+        first_name: 'Jill',
+        last_name: 'Jackson',
+        email: 'jilljackson@gmail.com'
+    }
+]
+
 
 /* GET root route: */
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.render('index', {
+        title: 'customers',
+        users: users
+    });
 });
 
 
