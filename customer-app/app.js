687987d324f71af4ef088328a99714929d846beb
“John <johnjamesmartin@live.com>
2016-10-21 04:41:39 +0100

    Forms and input

--- customer-app/app.js
+++ customer-app/app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var bodyParser =  require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 
 var app = express();
@@ -33,6 +34,37 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/* Global vars */
+
+app.use(function(req, res, next) {
+    res.locals.errors = null;
+    next();
+});
+
+
+/* Express validator middleware: */
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+/* Users array: */
+
 var users = [
     {
         id: 1,
@@ -62,6 +94,32 @@ app.get('/', function(req, res) {
         title: 'customers',
         users: users
     });
+});
+
+
+/* POST users/add: */
+
+app.post('/users/add', function(req, res) {
+    req.checkBody('first_name', 'First name is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {
+            title: 'customers',
+            users: users,
+            errors: errors
+        });
+    } else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        };
+        console.log('Success');
+    }
 });
 
 
